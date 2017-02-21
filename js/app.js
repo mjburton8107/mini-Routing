25a27dd86c278b7c725adaca37ac2274acf09fb9
Meidi <HaynieMac@MikeHeids-iMac.local>
2017-02-21 15:38:40 -0700

    ready to start changing routes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-angular.module('miniRouting', ['ui.router']);
+angular.module('miniRouting', ['ui.router'])
+
+.config(function($stateProvider, $urlRouterProvider){
+
+  $stateProvider
+
+  .state('home', {
+    templateUrl: "/home/homeTmpl.html",
+    controller: "homeCtrl",
+    url: '/'
+  })
+  .state('settings', {
+    templateUrl: "/settings/settingsTmpl.html",
+    controller: "settingsCtrl.js",
+    url: "/settings"
+  })
+  .state('products', {
+    templateUrl: "/products/productTmpl.html",
+    controller: "productsCtrl.js",
+    url: "/products/:id"
+  });
+
+  $urlRouterProvider.otherwise('/');
+
+})
